dc840852d491ce627708b7bb1e53dc75ec2e5f47
Scarlett <76939409+irsyadfata99@users.noreply.github.com>
2025-09-22 11:43:18 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,18 +17,15 @@ export default function RootLayout({
     <html lang="id">
       <head>
         {/* Google Analytics */}
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_title: document.title,
-              page_location: window.location.href,
-            });
-          `}
-        </Script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FYLD2GDD4F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FYLD2GDD4F');
+</script>
       </head>
       <body className="antialiased">{children}</body>
     </html>
